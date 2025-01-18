79336ce7267a2b78b95c976f2d6192a9b5a47ff3
lukitas06 <lucasalegre13@gmail.com>
2025-01-18 19:02:07 -0300

    fixing styles

--- src/app/projects/page.tsx
+++ src/app/projects/page.tsx
@@ -14,11 +14,11 @@ type Project = {
 export default async function Project() {
 
     return (
-        <div className="flex flex-col text-white p-8">
+        <div className="flex flex-col text-white p-2 md:p-8 my-7">
             <div className='cards-container flex flex-col gap-7 mb-20 '>
-                <h1 className='text-4xl font-semibold'>Simple projects</h1>
-                <div className='w-auto inline-flex flex-nowrap mx-10 overflow-hidden [mask-image:_linear-gradient(to_right,transparent_0,_black_128px,_black_calc(100%-200px),transparent_100%)]'>
-                    <div className='w-auto flex items-center justify-center md:justify-start group-hover:animation-pause [&_.projectCard]:mx-8 [&_img]:max-w-none animate-infinite-scroll '>
+                <h1 className='text-4xl font-semibold text-center md:text-start'>Simple projects</h1>
+                <div className='w-auto inline-flex flex-nowrap mx-2 md:mx-10 overflow-hidden [mask-image:_linear-gradient(to_right,transparent_0,_black_128px,_black_calc(100%-200px),transparent_100%)]'>
+                    <div className='w-auto flex items-center justify-center md:justify-start group-hover:animation-pause [&_.projectCard]:mx-8 md:[&_.projectCard]:mx-8 [&_img]:max-w-none animate-infinite-scroll '>
                         {simpleProjects.map((project: Project, index) => (
                             <ProjectCard
                                 key={index}
@@ -45,8 +45,8 @@ export default async function Project() {
                 </div>
             </div>
             <div className='cards-container flex flex-col gap-7'>
-                <h1 className='text-4xl font-semibold'>Cibersecurity projects</h1>
-                <div className='w-auto inline-flex flex-nowrap mx-10 overflow-hidden [mask-image:_linear-gradient(to_right,transparent_0,_black_128px,_black_calc(100%-200px),transparent_100%)]'>
+                <h1 className='text-4xl font-semibold text-center md:text-start'>Cibersecurity projects</h1>
+                <div className='w-auto inline-flex flex-nowrap mx-2 md:mx-10 overflow-hidden [mask-image:_linear-gradient(to_right,transparent_0,_black_128px,_black_calc(100%-200px),transparent_100%)]'>
                     <div className='w-auto flex items-center justify-center md:justify-start group-hover:animation-pause [&_.projectCard]:mx-8 [&_img]:max-w-none animate-infinite-scroll '>
                         {securityProjects.map((project, index) => (
                             <ProjectCard
